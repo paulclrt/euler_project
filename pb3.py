fd04ff55f1ec1f8d2dbdc021ce4b0636e11b977b
paulclrt <p4ul.claret@gmail.com>
2025-03-25 18:30:17 +0100

    fix problem 3

--- pb3.py
+++ pb3.py
@@ -1,17 +1,3 @@
-def fib(pre, next):
-    cur = pre + next
-    if cur > 4000000:
-        return 0
-    else:
-        if cur% 2 == 0:
-            print(cur)
-            return cur+ fib(next, cur)
-        else:
-            return fib(next, cur)
-
-
-# print("final: ", 2+fib(1, 2))
-
 def is_prime(num):
     is_prime = True
     for i in range(1, int(num/2)):
